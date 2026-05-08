8932aab5051ae36786ee05d0139ad4a0c9f36d2f
HABUMUGISHA Ghadi <ghadihabumugisha@gmail.com>
2026-05-08 10:26:30 +0200

    assignment submitted

--- Balanced.java
+++ Balanced.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 import java.util.Stack;
 public class Balanced{
     public static void main(String[] args) {
@@ -7,8 +8,23 @@ public class Balanced{
         
     }
     public static boolean isBalanced(String s){
-        //
-        
+        HashMap<Character, Character> map = new HashMap<>();
+        map.put('(', ')');
+        map.put('{', '}');
+        map.put('[', ']');
+
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (map.containsKey(c)) {
+                stack.push(c);
+            } else if (map.containsValue(c)) {
+                if (!stack.isEmpty() && map.get(stack.pop()) == c) {
+                    return true;
+                }
+            }
+        }
+
         return false;
 }
         
